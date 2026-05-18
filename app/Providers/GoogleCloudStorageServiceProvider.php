588297f7462d005eceb895e0a40637888b94fc88
Demi Oyewusi <tayodemi@gmail.com>
2026-05-18 01:14:17 +0100

    update keys

--- app/Providers/GoogleCloudStorageServiceProvider.php
+++ app/Providers/GoogleCloudStorageServiceProvider.php
@@ -8,7 +8,7 @@
 
 namespace App\Providers;
 
-use App\Filesystem\GcsFilesystemAdapter;
+use App\FileSystem\GcsFilesystemAdapter;
 use Google\Cloud\Storage\StorageClient;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 mkdir -p storage/framework/cache storage/framework/sessions storage/framework/views bootstrap/cache
 
 if [ "${RUN_MIGRATIONS:-false}" = "true" ]; then
-    php artisan migrate:fresh 
+    php artisan migrate:fresh --seed --force
 fi
 
 if [ "${CACHE_CONFIG:-true}" = "true" ]; then
